b5c0c0d469a94d725b6bae7eb89383744117009d
Ego <87696724+Egofir@users.noreply.github.com>
2022-10-21 12:04:14 +0800

    the third commit

--- src/main/java/com/yang/controller/Register.java
+++ src/main/java/com/yang/controller/Register.java
@@ -2,10 +2,13 @@ package com.yang.controller;
 
 import com.yang.mapper.UserMapper;
 import com.yang.pojo.User;
+import com.yang.util.PasswordUtil;
+import com.yang.util.SensitiveWordUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -13,17 +16,30 @@ public class Register {
     @Autowired
     private UserMapper userMapper;
 
-    @GetMapping("/queryUserList")
-    public List<User> queryUserList() {
-        List<User> userList = userMapper.queryUserList();
-        return userList;
-    }
-
     @RequestMapping("/ferry/register")
     public String register(@RequestParam("username") String username,
                            @RequestParam("password") String password,
                            Model model) {
-        userMapper.addUser(new User(null , username, password, null, null));
-        return "ok";
+        List<String> list = new ArrayList<>();
+        list.add("尼玛");
+        list.add("站长");
+        list.add("国家领导人");
+        list.add("操");
+        SensitiveWordUtil.initMap(list);
+        if (username.length() <= 15) {
+            if (PasswordUtil.checkPassword(password)) {
+                if (!SensitiveWordUtil.mathWords(username)) {
+                    if (userMapper.queryUserByUsername(username) == null) {
+                        userMapper.addUser(new User(null , username, password, null, null));
+                        return "ok";
+                    } else {
+                        return "already exist";
+                    }
+                }
+                return "用户名含有违规词汇";
+            }
+            return "密码强度不符";
+        }
+        return "用户名过长";
     }
 }
--- src/main/java/com/yang/mapper/UserMapper.java
+++ src/main/java/com/yang/mapper/UserMapper.java
@@ -1,12 +1,10 @@
 package com.yang.mapper;
 
 import com.yang.pojo.User;
-import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-//@Mapper
 @Repository
 public interface UserMapper {
     List<User> queryUserList();
